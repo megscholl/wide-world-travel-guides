f53ec7d3ffc7870f49cc04e7c7b60488691e6cbf
Meg Scholl <meghan.e.scholl@gmail.com>
2018-04-10 14:34:11 -0500

    travel icon changed, app js incorporates the object with the travel destinations

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Head from './Header';
 import Body from './Body';
 import Footer from './Footer';
-import logo from './images/galaxy1.png';
+import logo from './images/travel.png';
 import newEnglandSites from './sites'
 
 class App extends Component {
